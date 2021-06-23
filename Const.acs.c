623f33a601b4ee435bcedcc7dcea65436c867a86
AqwerfNet <coucou48500@yahoo.fr>
2021-06-24 06:16:32 +0900

    12B

--- Const.acs.c
+++ Const.acs.c
@@ -14,21 +14,22 @@
 #libdefine  MotherZombieMinHealth 4000
 #libdefine  ZombieMinimumHealth 1250 
 #libdefine  ZombieMaximumHealth  3000
-#libdefine  FastZombieMaximumHealth 2000
-#libdefine  FastZombieMinimumHealth 750
+
 #libdefine  HeavyZombieMaximumHealth 5000
 #libdefine  HeavyZombieMinimumHealth 2000
 
 #libdefine  ZOMBIESPEED 1.10
 #libdefine  MOTHERZOMBIESPEED 1.15
-#libdefine  FASTZOMBIESPEED 1.20
-#libdefine  HEAVYZOMBIESPEED 0.90
+#libdefine  FASTZOMBIESPEED 1.35
+#libdefine  HEAVYZOMBIESPEED 0.80
 
-str tooltips[8]=   {"Pistol fire rate may be slow,but its always accurate",
-					"If you fall behind as a Human, you may get easily infected!",
-					"In ESCAPE Maps, and as a zombie, you can Teleport to the lastest checkpoint available!",
-					"The more you aim with the sniper, the more its accurate and does damage!",
+#libdefine PI 3.14159265358979323846264338
+
+str tooltips[8]=   {"Pistol fire rate may be slow,\nbut its always accurate",
+					"If you fall behind as a Human, \nyou may get easily infected!",
+					"In ESCAPE Maps, and as a zombie, \nyou can Teleport to the lastest checkpoint available!",
+					"The more you aim with the sniper, \nthe more its accurate and does damage!",
 					"Rockets Stunts Zombies!",
 					"You dont lose your crates when you die!",
 					"If you are stuck, use the suicide command!",
-					"As a zombie, in escape maps, you can teleport to the start of the map!"};
+					"As a zombie, in escape maps, \nyou can teleport to the start of the map!"};
