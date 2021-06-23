f72ca9da1b17377cf79d9328ed85b8adf9b4843f
AqwerfNet <coucou48500@yahoo.fr>
2021-06-24 06:16:50 +0900

    12B

--- RWKHUD.acs.c
+++ RWKHUD.acs.c
@@ -125,3 +125,59 @@ script 807(void) CLIENTSIDE{
 	SETFONT("HUDINFEC");
 	HudMessage(s:"A"; HUDMSG_FADEINOUT, 306, CR_UNTRANSLATED, 0.5, 0.5, 0.5,0.5, 0.75);
 }
+
+script 808(void) NET CLIENTSIDE{
+
+	for(int time=0;time<35*5;time++){
+	//while(true){
+		int cur_x=GetActorX(activatorTID());
+		int cur_y=GetActorY(activatorTID());
+		int cur_z=GetActorZ(activatorTID());
+		int dist_min=1<<25;
+		int actual_i=0;
+		for(int i=0;i<64;i++){//Spot the nearest human, Because its freaking OP... imagine having 20 spotter zm in 1 round... LOL
+			if(CheckActorInventory(i+PLAYER_ID,"IsHuman")==1 && i+PLAYER_ID!=activatorTID()){
+				int enn_x=GetActorX(i+PLAYER_ID);
+				int enn_y=GetActorY(i+PLAYER_ID);
+				int enn_z=GetActorZ(i+PLAYER_ID);
+				//int cur_dist=ThreeD_Dist(enn_x-cur_x,enn_y-cur_y,enn_z-cur_z);//The nearest in a 3D space
+				int cur_dist=TwoD_Dist(enn_x-cur_x,enn_y-cur_y);
+				if(cur_dist<dist_min){
+					dist_min=cur_dist;
+					actual_i=i; 
+				}
+			}
+		}
+		
+		int vang = VectorAngle (GetActorX(actual_i+PLAYER_ID)-cur_x,GetActorY(actual_i+PLAYER_ID)-cur_y);
+        int angle = (vang-GetActorAngle(ActivatorTID()) + 1.0) % 1.0;
+		
+		if(((vang+0.125)%0.5) > 0.25) dist_min = fixeddiv(GetActorY(actual_i+PLAYER_ID)-cur_y, sin(vang));
+		else dist_min = fixeddiv(GetActorX(actual_i+PLAYER_ID)-cur_x, cos(vang));
+		
+		
+		int pitch = VectorAngle(dist_min,GetActorZ(actual_i+PLAYER_ID)-(cur_z + 32.0));
+		pitch = (pitch + GetActorPitch(ActivatorTID()) + 1.0) % 1.0;
+        if (angle < 0.2 || angle > 0.8)
+        {
+            int sx = 320 - (320 * Sin (angle) / Cos (angle));
+			int sy=0;
+			if(Cos (pitch)==0){
+				sy = 240 - (240 * Sin (pitch) / Cos (0.24));
+			}else{
+				sy = 240 - (240 * Sin (pitch) / Cos (pitch));
+			}
+            SetHudSize (640, 480, 0);
+			SetFont("HUDHEAD");
+            HudMessage (s:"A"; HUDMSG_PLAIN, 1, CR_RED, sx * 1.0, sy*1.0, 0);
+        }
+        else
+        {
+            HudMessage (s:""; HUDMSG_PLAIN, 1, 0, 0, 0, 0);
+        }
+
+        Delay(1);
+		}
+	HudMessage (s:""; HUDMSG_PLAIN, 1, CR_RED,0, 0, 0);
+	
+}
